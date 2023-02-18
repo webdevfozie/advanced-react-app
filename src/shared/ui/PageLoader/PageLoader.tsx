035fa7095ecb40c551b1bb1802594d07aa081737
Artem Frolov <af.fozie@gmail.com>
2023-02-18 20:31:54 +0600

    fix PageLoader.tsx

--- src/shared/ui/PageLoader/PageLoader.tsx
+++ src/shared/ui/PageLoader/PageLoader.tsx
@@ -1,6 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Loader } from 'shared/ui/Loader/Loader'
-import cls from './PageLoader.scss'
+import cls from './PageLoader.module.scss'
 
 interface PageLoaderProps {
   className?: string,
