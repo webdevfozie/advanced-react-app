9e3bbdfaba2e3e036ffbc2cf4ae3bc1410c5170a
Artem Frolov <af.fozie@gmail.com>
2023-02-25 14:42:58 +0600

    router in test

---
+++ src/shared/lib/testing/componentRender/componentRender.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react'
+import { render } from '@testing-library/react'
+import { I18nextProvider } from 'react-i18next'
+import i18nForTesting from 'shared/config/i18n/i18nForTesting'
+import { MemoryRouter } from 'react-router-dom'
+
+export interface ComponentRenderOptions {
+  route?: string
+}
+
+export function componentRender(component: ReactNode, { route = '/' }: ComponentRenderOptions = {}) {
+  return render(
+    <MemoryRouter initialEntries={[route]}>
+      <I18nextProvider i18n={i18nForTesting}>
+        {component}
+      </I18nextProvider>
+    </MemoryRouter>,
+  )
+}
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react'
-import { renderWithTranslation } from 'shared/lib/testing/renderWithTranslation/renderWithTranslation'
+import { componentRender } from 'shared/lib/testing/componentRender/componentRender'
 import { Sidebar } from './Sidebar'
 
 describe('Sidebar', () => {
   test('render', () => {
-    renderWithTranslation(<Sidebar />)
+    componentRender(<Sidebar />)
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
   })
 
   test('toggle', () => {
-    renderWithTranslation(<Sidebar />)
+    componentRender(<Sidebar />)
     const toggle = screen.getByTestId('sidebar-toggle')
     fireEvent.click(toggle)
     expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
