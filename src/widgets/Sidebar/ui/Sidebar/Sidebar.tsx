2c15b077741cf1b3078333e6672e2809c6769ddb
Artem Frolov <af.fozie@gmail.com>
2023-02-25 09:55:08 +0600

    fix translations

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher'
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher'
+import { useTranslation } from 'react-i18next'
 import cls from './Sidebar.module.scss'
 
 interface SidebarProps {
@@ -10,6 +11,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false)
+  const { t } = useTranslation()
 
   function toggle() {
     setCollapsed((prevState) => !prevState)
@@ -25,7 +27,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
         type="button"
         onClick={toggle}
       >
-        toggle
+        {t('Переключить')}
       </button>
 
       <div className={classNames(cls.switchers)}>
