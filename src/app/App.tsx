8f66e6bb2affd3d4478cbfda019a7c7944faa709
Artem Frolov <af.fozie@gmail.com>
2023-03-05 16:29:27 +0600

    refactoring themes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { AppRouter } from 'app/providers/router'
 import { Navbar } from 'widgets/Navbar'
@@ -6,10 +5,8 @@ import { Sidebar } from 'widgets/Sidebar'
 import { Suspense } from 'react'
 
 function App() {
-  const { theme } = useTheme()
-
   return (
-    <div className={classNames('app', {}, [theme])}>
+    <div className={classNames('app', {}, [])}>
       <Suspense fallback="">
         <Navbar />
         <div className="content-page">
