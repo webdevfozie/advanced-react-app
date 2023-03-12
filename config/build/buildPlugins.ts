706ff5aae7335db52db0976623514b8daa0babac
Artem Frolov <af.fozie@gmail.com>
2023-03-12 16:51:46 +0600

    return bundle analyzer in dev mode

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -22,12 +22,11 @@ export function buildPlugins(options: BuildOptions): WebpackPluginInstance[] {
     }),
   ]
 
-  plugins.push(new BundleAnalyzerPlugin({
-    openAnalyzer: false,
-  }))
-
   if (isDev) {
     plugins.push(new HotModuleReplacementPlugin())
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }))
   }
 
   return plugins
